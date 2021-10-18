962817480a295f2d5965b8790254014ff218dba7
Eduard Kiz <edik.kiz99@gmail.com>
2021-10-18 09:35:43 +0300

    deploy

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ app.use(cors({
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(`client/build`));
   app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
